e522939d3ebf46269c54e5ee9c373cded5fd24a3
Richard <richard.neher@tuebingen.mpg.de>
2014-12-01 13:13:15 +0100

    added webcam

---
+++ python_src/webcam_surveillance.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import os
+import argparse
+import subprocess
+import datetime
+import time
+
+parser = argparse.ArgumentParser(description="record the running morbidostat")
+parser.add_argument('--dt', type=int, default = 600, help = 'number seconds between pictures')
+parser.add_argument('--outdir', type=str, default='.', help = 'directory for output files')
+params=parser.parse_args()
+
+
+cams = ['video1', 'video2']
+dt = max(params.dt,15)  # seconds between pictures
+outdir = params.outdir.rstrip('/')+'/'
+
+# make directory if it does not exist
+if not os.path.exists(outdir):
+    os.mkdir(outdir)
+
+pic_count = 0
+while pic_count<10000: # loop forever (until 10000 pictures are taken)
+    # make date string to label files
+    now = datetime.datetime.now()
+    now_str = now.strftime('%Y%m%d_%H-%M-%S')
+    for cam in cams:
+        args = ['-c', '/dev/'+cam, '-o', outdir+cam+'_'+now_str+'.jpeg']
+        subprocess.call(['streamer']+args)
+        time.sleep(5)  # allow 5 seconds for the picture taking
+    time.sleep(params.dt-5*len(cams)) # wait before taking next picture
+    pic_count +=1 
+
