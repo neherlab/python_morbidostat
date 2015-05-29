9a7504fe5955432019f6c4e9fbae8363f4b8525e
morbidostat <morbidostat@ag-neher-labor1>
2015-05-29 12:35:19 +0200

    pump calibration, new camera script

--- python_src/webcam_surveillance.py
+++ python_src/webcam_surveillance.py
@@ -4,14 +4,14 @@ import argparse
 import subprocess
 import datetime
 import time
+import glob
 
 parser = argparse.ArgumentParser(description="record the running morbidostat")
 parser.add_argument('--dt', type=int, default = 600, help = 'number seconds between pictures')
 parser.add_argument('--outdir', type=str, default='.', help = 'directory for output files')
 params=parser.parse_args()
 
-
-cams = ['video3'] #, 'video3']
+#cams = ['video1', 'video2']
 dt = max(params.dt,15)  # seconds between pictures
 outdir = params.outdir.rstrip('/')+'/'
 
@@ -24,10 +24,13 @@ while pic_count<10000: # loop forever (until 10000 pictures are taken)
     # make date string to label files
     now = datetime.datetime.now()
     now_str = now.strftime('%Y%m%d_%H-%M-%S')
-    for cam in cams:
-        args = ['-c', '/dev/'+cam, '-s 640x480', '-o', outdir+cam+'_'+now_str+'.jpeg']
+    camlist = glob.glob('/dev/video*')
+    for cam in camlist:
+        camname = cam.replace('/dev/', '')
+        args = ['-c', cam, '-s 640x480', '-o', outdir+camname+'_'+now_str+'.jpeg']
+        print ' '.join(['streamer']+args)
         subprocess.call(['streamer']+args)
         time.sleep(10)  # allow 5 seconds for the picture taking
-    time.sleep(params.dt-10*len(cams)) # wait before taking next picture
+    time.sleep(params.dt-10*len(camlist)) # wait before taking next picture
     pic_count +=1 
 
